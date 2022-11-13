caa0d403be68a0856b534253fe2187387e2adfe1
Ahmad adnan Abdullah <70108548+abah19vo@users.noreply.github.com>
2022-11-13 22:39:47 +0100

    uploaded projekt

---
+++ Landmark2/Landmark2/Landmark2App.swift
@@ -0,0 +1,17 @@
+//
+//  Landmark2App.swift
+//  Landmark2
+//
+//  Created by Ahmad Adnan Abdullah on 2022-11-12.
+//
+
+import SwiftUI
+
+@main
+struct Landmark2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
